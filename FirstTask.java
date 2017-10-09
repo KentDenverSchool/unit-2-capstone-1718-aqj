0ba2d7d43647edbc88146aa0fc795f40d5d5fb8c
8NW <awoodard19@kentdenver.org>
2017-10-09 09:25:00 -0600

    Update FirstTask.java

--- FirstTask.java
+++ FirstTask.java
@@ -1,3 +1,30 @@
-public class FirstTask{
 
+import java.*;
+public class FirstTask<Key extends Comparable<Key>, Value>{
+
+    //add an key-value pair to the dictionary
+    void put(Key key, Value value){
+
+    }
+
+    //get the value associated with a given key
+    Value get(Key key){
+
+    }
+
+    //remove a key-value pair and return its value
+    Value remove(Key key){
+
+    }
+
+    //returns true if the dictionary is empty
+    boolean isEmpty(){
+
+    }
+
+    //returns the number of key-value pairs in the dictionary
+    int size(){
+
+    }
+  
 }
