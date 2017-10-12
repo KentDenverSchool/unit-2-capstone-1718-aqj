3801ff4820ff1ec6f9956cb5fb6ab387f99da0e4
joshyhasc <jhascall19@Jhascall19.local>
2017-10-12 08:46:04 -0600

    JOSHYBOI

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -5,32 +5,46 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
     private ArrayList<Value>[] a;
     private int size;
     private int m;
-    public FirstTask(){
+
+    public FirstTask() {
         m = 2147483647;
         a = new ArrayList[m];
     }
+
     //add an key-value pair to the dictionary
-    void put(Key key, Value value){
-size++;
+    void put(Key key, Value value) {
+
+        size++;
     }
-  //get the value associated with a given key
-    Value get(Key key){
+
+    //get the value associated with a given key
+    Value get(Key key) {
 
     }
 
     //remove a key-value pair and return its value
-    Value remove(Key key){
-size--;
+    Value remove(Key key) {
+
+        size--;
     }
 
     //returns true if the dictionary is empty
-    boolean isEmpty(){
-if (size() == 0){
-    return true;
-}else return false;
+    boolean isEmpty() {
+        if (size() == 0) {
+            return true;
+        } else return false;
     }
 
+    int hashKey(Key key) {
+        String stringKey = key.toString();
+    int hashedStringKey = stringKey.hashCode();
+
+        return (hashedStringKey % m);
+    }
+
+
     //returns the number of key-value pairs in the dictionary
-    int size(){
-    return size;
+    int size() {
+        return size;
+    }
 }
