8d6501b8457a49fa15dfb755dec08910666ce1f2
joshyhasc <jhascall19@Jhascall19.local>
2017-10-12 08:39:02 -0600

    I hope this works

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -2,8 +2,8 @@ import java.*;
 import java.util.ArrayList;
 
 public class FirstTask<Key extends Comparable<Key>, Value> {
-
     private ArrayList<Value>[] a;
+    private int size;
     private int m;
     public FirstTask(){
         m = 2147483647;
@@ -11,7 +11,7 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
     }
     //add an key-value pair to the dictionary
     void put(Key key, Value value){
-
+size++;
     }
   //get the value associated with a given key
     Value get(Key key){
@@ -20,17 +20,17 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
 
     //remove a key-value pair and return its value
     Value remove(Key key){
-
+size--;
     }
 
     //returns true if the dictionary is empty
     boolean isEmpty(){
-
+if (size() == 0){
+    return true;
+}else return false;
     }
 
     //returns the number of key-value pairs in the dictionary
     int size(){
-
-        
-    }
+    return size;
 }
