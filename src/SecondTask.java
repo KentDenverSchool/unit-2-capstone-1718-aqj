e4362f3cd222d347da35d3b8a14dc36f01b62f9c
joshyhasc <jhascall19@Jhascall19.local>
2017-10-16 13:57:11 -0600

    Pull through

---
+++ src/SecondTask.java
@@ -0,0 +1,13 @@
+/**
+ * Created by jhascall19 on 10/16/17.
+ */
+public class SecondTask extends FirstTask{
+    //Updates m to the new value. Rehashes all keys
+
+    public void changeM(int newM){
+
+    }
+    public void resize(int newM){
+
+    }
+}
