5379863ff0a5a7b3259049dfd95764961204a40e
QVoboril18 <qvoboril18@kentdenver.org>
2017-10-16 13:46:28 -0600

    put written
    Quinn wrote put

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -2,20 +2,26 @@ import java.*;
 import java.util.ArrayList;
 
 public class FirstTask<Key extends Comparable<Key>, Value> {
-
     private ArrayList<Value>[] a;
+    private int size;
     private int m;
+
     public FirstTask(){
         m = 2147483647;
         a = new ArrayList[m];
     }
+
     //add an key-value pair to the dictionary
     void put(Key key, Value value){
-
+        int Hashedkey = hashKey(key);
+        a[Hashedkey].add(value);
+        size++;
     }
-  //get the value associated with a given key
+    //get the value associated with a given key
     Value get(Key key){
 
+        int hashKey = hashKey(key);
+        return a[hashKey].get(0);
     }
 
     //remove a key-value pair and return its value
@@ -28,9 +34,16 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
 
     }
 
-    //returns the number of key-value pairs in the dictionary
-    int size(){
 
-        
+
+    int hashKey(Key key){
+
+        String stringKey = key.toString();
+        int hashedStringKey = stringKey.hashCode();
+
+        return (hashedStringKey % m);
+
     }
+    //returns the number of key-value pairs in the dictionary
+    int size(){ }
 }
