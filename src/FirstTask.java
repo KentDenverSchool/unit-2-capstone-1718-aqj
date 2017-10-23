6e3a6f3292632a4037c284a0cd22bf5a2f8f616d
joshyhasc <jhascall19@Jhascall19.local>
2017-10-23 09:38:30 -0600

    XSDFCGHVJLHV
    THIS IS WHY WE CANT HAVE NICE THINGS!

--- src/FirstTask.java
+++ src/FirstTask.java
@@ -6,7 +6,7 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
     private int size;
     private int m;
 
-    public FirstTask(){
+    public FirstTask() {
 
         m = 2147483647;
         a = new ArrayList[m];
@@ -14,20 +14,21 @@ public class FirstTask<Key extends Comparable<Key>, Value> {
 
     //add an key-value pair to the dictionary
 
-    void put(Key key, Value value){
+    void put(Key key, Value value) {
         int Hashedkey = hashKey(key);
         a[Hashedkey].add(value);
         size++;
     }
+
     //get the value associated with a given key
-    Value get(Key key){
+    Value get(Key key) {
         int hashKey = hashKey(key);
         return a[hashKey].get(0);
     }
 
     //remove a key-value pair and return its value
     Value remove(Key key) {
-int hashedKey = hashKey(key);
+        int hashedKey = hashKey(key);
         a[hashedKey] = new ArrayList<Value>();
         size--;
     }
@@ -39,23 +40,32 @@ int hashedKey = hashKey(key);
         } else return false;
     }
 
-    int hashKey(Key key) {
-        String stringKey = key.toString();
-    int hashedStringKey = stringKey.hashCode();
 
-        return (hashedStringKey % m);
-    }
     //returns the number of key-value pairs in the dictionary
     int size() {
         return size;
+    }
 
 
-    int hashKey(Key key){
+    int hashKey(Key key) {
 
         String stringKey = key.toString();
         int hashedStringKey = stringKey.hashCode();
 
         return (hashedStringKey % m);
     }
+
+    // TASK 2: THE SECOND COMING OF CHRIST
     
+    public void resize(int newM){
+        m = newM;
+        ArrayList<Value>[] aCopy = new ArrayList[m];
+
+        for (int i = 0; i < a.length; i++) {
+            int newI =
+
+        }
+    }
+
 }
+
